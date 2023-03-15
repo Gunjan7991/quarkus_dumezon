60eed961df98a615b39dcba0209a6ae548c563f2
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-03-15 18:21:21 -0500

    re_download with starter code

---
+++ src/test/java/com/gunjan/code/prac/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.gunjan.code.prac;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
